9ce50d5035ec883d73c935e211c17973682d9a83
lingzhic <lingzhic@student.unimelb.edu.au>
2022-10-19 04:05:35 +1100

    initial

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qmj39gjz%oyg6!dme1n@#m491mks58^iyvk%pxmv(dd*do7!ms"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["diffusiondata.herokuapp.com"]
 
@@ -122,4 +122,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
