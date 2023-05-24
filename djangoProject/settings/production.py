5ab1590617ad464fd786dc913868e6796c22c2dc
lingzhic <lingzhic@student.unimelb.edu.au>
2023-05-24 19:23:40 +1000

    update

--- djangoProject/settings/production.py
+++ djangoProject/settings/production.py
@@ -4,4 +4,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["3.26.13.67", "127.0.0.1:8000", "*"]
+ALLOWED_HOSTS = ["3.26.13.67", "172.31.19.148", "127.0.0.1:8000", "*"]
